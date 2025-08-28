df6f9ff748890cd661df9b7c313bd19f6dc4c815
lucasgfeldmann <lucasgfeldmann@gmail.com>
2025-08-28 20:03:29 -0300

    feat: add function to return all paths

---
+++ src/get_all_files_path.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+from typing import Generator
+
+
+def get_all_files_path(directory: str | Path) -> Generator[Path, None, None]:
+    """Gera o caminho de cada arquivo em um diretório, de forma recursiva."""
+
+    path_obj = Path(directory)
+
+    for path in path_obj.rglob("*"):
+        if path.is_file():
+            yield path
+
+
+if __name__ == "__main__":
+    paths_generator = get_all_files_path("/home/lucas/Projects/file-organizer")
+
+    for path in paths_generator:
+        print(path)
