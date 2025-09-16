8a781637e40a579a329d55df014e49c39553d5ed
lucasgfeldmann <lucasgfeldmann@gmail.com>
2025-09-16 14:48:14 -0300

    fix: get all file from the current directory not a code fixed value

--- src/get_all_files_path.py
+++ src/get_all_files_path.py
@@ -13,7 +13,7 @@ def get_all_files_path(directory: str | Path) -> Generator[Path, None, None]:
 
 
 if __name__ == "__main__":
-    paths_generator = get_all_files_path("/home/lucas/Projects/file-organizer")
-
+    paths_generator = get_all_files_path(Path.cwd())
+    
     for path in paths_generator:
         print(path)
